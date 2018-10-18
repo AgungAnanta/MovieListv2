d9a3dc212ae4505cd84414890f1116b59c8c8f83
AgungAnanta <agunganantawijaya@student.upi.edu>
2018-10-18 07:44:43 +0700

    update

--- app/src/main/java/com/tugas/ignagungaaw1600872/movielist/detail.java
+++ app/src/main/java/com/tugas/ignagungaaw1600872/movielist/detail.java
@@ -7,16 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class detail extends AppCompatActivity {
-    RecyclerView rvCategory;
-    private ArrayList<Movie> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        rvCategory = (RecyclerView)findViewById(R.id.rv_category);
-        rvCategory.setHasFixedSize(true);
-        list = new ArrayList<>();
-        list.addAll(MovieData.getListData());
-        showRecyclerCardView();
     }
 }
